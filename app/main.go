25ee4a5ad8f2afccd3b99b42372a56b5f167b678
ThiccPan <ihilmiderian@gmail.com>
2023-05-01 21:31:53 +0700

    add initial psql gorm config

--- app/main.go
+++ app/main.go
@@ -5,10 +5,21 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	gormPsqlConf "github.com/thiccpan/go-online-lawyer/app/config"
 )
 
 func main() {
 	e := echo.New()
+
+	DB := gormPsqlConf.ConfigDB{
+		DB_Username: "thiccpan",
+		DB_Password: "dbpsqlpass432",
+		DB_Port:     "5432",
+		DB_Host:     "localhost",
+		DB_Name:     "go_online_lawyer",
+	}
+
+	DB.InitDB()
 
 	// Middleware
 	e.Use(middleware.Logger())
