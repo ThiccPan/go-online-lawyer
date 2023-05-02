bd2544ac4fe722669fd9718b0c1b43435e00f44f
ThiccPan <ihilmiderian@gmail.com>
2023-05-02 13:01:16 +0700

    add pengacara config

--- app/main.go
+++ app/main.go
@@ -6,12 +6,15 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	gormPsqlConf "github.com/thiccpan/go-online-lawyer/app/config"
+	"github.com/thiccpan/go-online-lawyer/controllers"
+	"github.com/thiccpan/go-online-lawyer/storage"
+	"github.com/thiccpan/go-online-lawyer/usecases"
 )
 
 func main() {
 	e := echo.New()
 
-	DB := gormPsqlConf.ConfigDB{
+	DBconf := gormPsqlConf.ConfigDB{
 		DB_Username: "thiccpan",
 		DB_Password: "dbpsqlpass432",
 		DB_Port:     "5432",
@@ -19,7 +22,11 @@ func main() {
 		DB_Name:     "go_online_lawyer",
 	}
 
-	DB.InitDB()
+	DB := DBconf.InitDB()
+
+	pengacaraStorage := storage.NewPengacaraStorer(DB)
+	pengacaraUseCase := usecases.New(pengacaraStorage)
+	pengacaraController := controllers.NewController(pengacaraUseCase)
 
 	// Middleware
 	e.Use(middleware.Logger())
@@ -27,6 +34,7 @@ func main() {
 	e.GET("/health", func(ctx echo.Context) error {
 		return ctx.JSON(http.StatusAccepted, "online")
 	})
+	e.GET("/pengacaras", pengacaraController.GetAll)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
