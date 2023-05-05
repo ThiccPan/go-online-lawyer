f51cd974f91bc14244d4a437a322edc9fa054cd7
ThiccPan <ihilmiderian@gmail.com>
2023-05-05 22:04:47 +0700

    konsultasi entities

--- entities/konsultasi.go
+++ entities/konsultasi.go
@@ -1,9 +1,25 @@
 package entities
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"github.com/thiccpan/go-online-lawyer/constants"
+	"gorm.io/gorm"
+)
 
 type Konsultasi struct {
 	gorm.Model
-	UserId uint
-	PengacaraId uint
+	UserId uint `json:"userId" validate:"required"`
+	PengacaraId uint `json:"pengacaraId" validate:"required"`
+	Status constants.KonsultasiStatus `validate:"required" gorm:"konsultasiStatus"`
+	KonsultasiTime time.Time `json:"konsultasiTime" validate:"required"`
+	Link string `json:"link" validate:"required" gorm:"type:text"`
+}
+
+type KonsultasiDTO struct {
+	UserId uint `json:"userId" validate:"required"`
+	PengacaraId uint `json:"pengacaraId" validate:"required"`
+	Status constants.KonsultasiStatus `validate:"required" gorm:"konsultasiStatus"`
+	KonsultasiTime string `json:"konsultasiTime" validate:"required"`
+	Link string `json:"link" validate:"required" gorm:"type:text"`
 }
