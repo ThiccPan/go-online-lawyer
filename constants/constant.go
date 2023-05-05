ae6141776497f5717a3451b4c90d8c7aef98bd6a
ThiccPan <ihilmiderian@gmail.com>
2023-05-05 17:57:49 +0700

    constant for status konsultasi

--- constants/constant.go
+++ constants/constant.go
@@ -1,3 +1,12 @@
 package constants
 
 const JWT_SECRET = "secret"
+
+type KonsultasiStatus string
+
+const (
+	PEMBAYARAN KonsultasiStatus = "PEMBAYARAN"
+	DIPROSES KonsultasiStatus = "DIPROSES"
+	SELESAI KonsultasiStatus = "SELESAI"
+	BATAL KonsultasiStatus = "DIBATALKAN"
+)
